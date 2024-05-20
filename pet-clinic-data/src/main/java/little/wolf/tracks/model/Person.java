27be62bbf9b44539a0776672cbaea3f8708b69c5
Amy Wolf <amy.m.wolf8.civ@army.mil>
2024-05-20 09:05:41 -0400

    add base entity. closes #10

--- pet-clinic-data/src/main/java/little/wolf/tracks/model/Person.java
+++ pet-clinic-data/src/main/java/little/wolf/tracks/model/Person.java
@@ -1,6 +1,6 @@
 package little.wolf.tracks.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/little/wolf/tracks/model/PetType.java
+++ pet-clinic-data/src/main/java/little/wolf/tracks/model/PetType.java
@@ -1,6 +1,6 @@
 package little.wolf.tracks.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
