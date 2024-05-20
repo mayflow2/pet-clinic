8b52b9a13356b85c263fc75a84ecb4811683efdb
Amy Wolf <amy.m.wolf8.civ@army.mil>
2024-05-20 08:57:46 -0400

    create interfaces for services. closes #5

---
+++ pet-clinic-data/src/main/java/little/wolf/tracks/services/PetService.java
@@ -0,0 +1,11 @@
+package little.wolf.tracks.services;
+
+import little.wolf.tracks.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/little/wolf/tracks/services/VetService.java
@@ -0,0 +1,11 @@
+package little.wolf.tracks.services;
+
+import little.wolf.tracks.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
