19db4bab1d63da7ab459cc0dde1a5bf400b14f85
Amy Wolf <amy.m.wolf8.civ@army.mil>
2024-05-20 16:39:46 -0400

    update map impl to manage setting of ID prop. closes #23

--- pet-clinic-data/src/main/java/little/wolf/tracks/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/little/wolf/tracks/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package little.wolf.tracks.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import little.wolf.tracks.model.BaseEntity;
 
-public class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -16,8 +15,14 @@ public class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+        if (object != null) {
+            if (object.getId() == null)
+                object.setId(getNextId());
+
+            map.put(object.getId(), object);
+        } else throw new RuntimeException("Object cannot be null");
+
         return object;
     }
 
@@ -25,7 +30,17 @@ public class AbstractMapService<T, ID> {
         map.remove(id);
     }
 
-    void delete(T object){
+    void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+        return nextId;
     }
 }
