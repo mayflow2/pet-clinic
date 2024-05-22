9d77d6073a7f464b4b5086213b0990249d53d8e6
Amy Wolf <amy.m.wolf8.civ@army.mil>
2024-05-22 09:20:15 -0400

    add specialties map. closes #31

---
+++ pet-clinic-data/src/main/java/little/wolf/tracks/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package little.wolf.tracks.services;
+
+import little.wolf.tracks.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/little/wolf/tracks/services/map/SpecialtyMapService.java
@@ -0,0 +1,37 @@
+package little.wolf.tracks.services.map;
+
+import little.wolf.tracks.model.Specialty;
+import little.wolf.tracks.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        super.delete(specialty);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return super.save(specialty);
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+}
