870923966f675551512c251b42b948ad967aba49
Amy Wolf <amy.m.wolf8.civ@army.mil>
2024-05-23 13:07:53 -0400

    implement map impl for visit service. closes #46

---
+++ pet-clinic-data/src/main/java/little/wolf/tracks/services/VisitService.java
@@ -0,0 +1,6 @@
+package little.wolf.tracks.services;
+
+import little.wolf.tracks.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/little/wolf/tracks/services/map/VisitMapService.java
@@ -0,0 +1,41 @@
+package little.wolf.tracks.services.map;
+
+import little.wolf.tracks.model.Visit;
+import little.wolf.tracks.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null
+                || visit.getPet().getOwner() == null
+                || visit.getPet().getId() == null)
+            throw new RuntimeException("Invalid Visit");
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+}
