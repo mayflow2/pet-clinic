e8f6ae5c87c03334539c0bb1047e164b6f326731
Amy Wolf <amy.m.wolf8.civ@army.mil>
2024-05-23 12:49:07 -0400

    add specialty sd jpa. closes #44

---
+++ pet-clinic-data/src/main/java/little/wolf/tracks/services/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,47 @@
+package little.wolf.tracks.services.springdatajpa;
+
+import little.wolf.tracks.model.Specialty;
+import little.wolf.tracks.repositories.SpecialtyRepository;
+import little.wolf.tracks.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Profile("springdatajpa")
+@Service
+public class SpecialtySDJpaService implements SpecialtyService {
+    private final SpecialtyRepository specialtyRepository;
+
+    public SpecialtySDJpaService(SpecialtyRepository specialtyRepository) {
+        this.specialtyRepository = specialtyRepository;
+    }
+
+    @Override
+    public Set<Specialty> findAll() {
+        Set<Specialty> specialties = new HashSet<>();
+        specialtyRepository.findAll().forEach(specialties::add);
+        return specialties;
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return specialtyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Specialty save(Specialty specialty) {
+        return specialtyRepository.save(specialty);
+    }
+
+    @Override
+    public void delete(Specialty specialty) {
+        specialtyRepository.delete(specialty);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialtyRepository.deleteById(id);
+    }
+}
